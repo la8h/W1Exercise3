04e5d0c82a9579aa8fa98ca900b59dcffc82ceed
la8h <50991583+la8h@users.noreply.github.com>
2022-03-26 19:50:25 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W1Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int random1, random2;
+            char ch = 'y';
+
+            while(ch == 'y')
+            {
+                random1 = rnd.Next(1, 10);
+                random2 = rnd.Next(1, 10);
+
+                int correctans = random1 + random2;
+                int userans;
+
+                do
+                {
+                    Console.Write("What is " + random1 + "+" + random2 + "?");
+
+                    userans = int.Parse(Console.ReadLine());
+
+                    if (userans == correctans)
+                        Console.WriteLine("Correct answer");
+                    else
+                        Console.WriteLine("Wrong answer");
+                } while (userans != correctans);
+
+                Console.Write("Would you like another question (y/n):");
+                ch = Console.ReadLine()[0];
+
+                }
+        }
+    }
+}
